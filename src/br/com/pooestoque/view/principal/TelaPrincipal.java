2aed5934d9e2ae06b3a552cd98a4156091e20528
augusto.ortolan <augusto.ortolan@PRC001.parceria.local>
2019-11-18 18:13:31 -0200

    Relatórios

--- src/br/com/pooestoque/view/principal/TelaPrincipal.java
+++ src/br/com/pooestoque/view/principal/TelaPrincipal.java
@@ -270,7 +270,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(jDesktopPane1)
         );
 
         pack();
